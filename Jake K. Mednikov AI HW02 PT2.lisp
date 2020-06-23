ec40c053bd4629d8cb8525582ce826586c4e866d
jmednikov <jmednikov13@gmail.com>
2020-06-23 14:41:22 -0700

    Add files via upload

---
+++ Jake K. Mednikov AI HW02 PT2.lisp
@@ -0,0 +1,15 @@
+;Jake K. Mednikov AI HW02 PT2
+
+(setq x '(a b c d))
+
+(setq y '(1 2 3 4))
+
+(print (reverse x))
+
+(print (list'6(third x) (second x )(second y)))
+
+(print (cons x (car y) ))
+
+(print  (list (car x) (third x) (car y) (third y) '(.a)))
+
+(print (list (car x) (car y) (second x ) (second y) (third x) (third y) (last x) (last y)))
